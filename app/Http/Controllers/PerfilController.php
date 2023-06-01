72e46b89ee013c0547bf46a2b07d16bed20554a5
Radhames2418 <Radhamesenc2412002@gmail.com>
2023-05-31 23:08:24 -0400

    Trabajando en el modulo de perfirles

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -18,5 +20,39 @@ class PerfilController extends Controller
     {
         $this->authorize('update', [$user, auth()->user()]);
         return view('perfil.index');
+    }
+
+    public function store(Request $request, User $user)
+    {
+        // Modificar el request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        // Validacion
+        $this->validate($request, [
+            'username' => [
+                'required',
+                'unique:users,username,'.auth()->user()->id,
+                'min:3',
+                'max:20',
+                'not_in:editar-perfil'
+            ]
+        ]);
+
+        if ($request->imagen)
+        {
+            $imagen = $request->file('imagen');
+
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+            $imagenServidor = Image::make($imagen);
+
+            $imagenServidor->fit(1000, 1000);
+
+            $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+
+            $imagenServidor->save($imagenPath);
+        }
+
+
     }
 }
